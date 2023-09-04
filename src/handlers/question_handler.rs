a48fd5d836d0ef3d979e76c5a37c9adc0d5fea7e
kherld-hussein <eternety11@gmail.com>
2023-09-04 19:02:05 +0300

    Refactor handers to one file & make crate public

--- src/handlers/question_handler.rs
+++ src/handlers/question_handler.rs
@@ -1,19 +1,32 @@
-use actix_web::{ web, HttpResponse };
-use crate::utils::cache::{ AppCache, get_from_cache, store_in_cache };
+use std::io::Read;
+use std::fs::File;
+use serde_json::Value;
 
-pub async fn get_cached_question(
-    cache: web::Data<AppCache>,
-    key: web::Path<String>
-) -> HttpResponse {
-    if let Some(value) = get_from_cache(&cache, &key) {
-        return HttpResponse::Ok().body(value);
-    }
+pub fn read_quiz_data() -> Result<Value, Box<dyn std::error::Error>> {
+    // Read the JSON file
+    let mut file = File::open("src/assets/quiz_data.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
 
-    // Retrieve the question from the database or other source
-    let question = "Sample question data".to_owned();
+    // Parse the JSON data into a serde_json::Value
+    let json_data: Value = serde_json::from_str(&contents)?;
 
-    // Store the question in the cache
-    store_in_cache(&cache, &key, question.clone()).await;
-
-    HttpResponse::Ok().body(question)
+    Ok(json_data)
 }
+
+// pub async fn get_cached_question(
+//     cache: web::Data<AppCache>,
+//     key: web::Path<String>
+// ) -> HttpResponse {
+//     if let Some(value) = get_from_cache(&cache, &key) {
+//         return HttpResponse::Ok().body(value);
+//     }
+
+//     // Retrieve the question
+//     let question = "Sample question data".to_owned();
+
+//     // Store the question in the cache
+//     store_in_cache(&cache, &key, question.clone()).await;
+
+//     HttpResponse::Ok().body(question)
+// }
