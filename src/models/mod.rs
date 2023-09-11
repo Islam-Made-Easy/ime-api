6db7315a4338ae88141bb6d25ec2db78a0c448df
kherld-hussein <eternety11@gmail.com>
2023-09-12 02:10:20 +0300

    Add month to the model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -4,6 +4,7 @@ use serde::{ Deserialize, Serialize };
 pub(crate) struct QueryParams {
     pub page: Option<usize>, // Page number
     pub limit: Option<usize>,
+    pub month: Option<String>,
     pub filter_fields: Option<String>,
     pub items_per_page: Option<usize>, // Items per page
 }
@@ -11,6 +12,7 @@ pub(crate) struct QueryParams {
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct QuizQuestion {
     pub id: u32,
+    pub month: String,
     pub question: String,
     pub question_type: String,
     pub answers: Vec<String>,
